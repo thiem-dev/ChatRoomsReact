2e5df3883f7d2b56feaf97e2f115dd239eac9eb6
Thiem <thiem.huynh0@gmail.com>
2024-01-21 15:41:14 -0500

    needs to figure msg structure for emitting to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3000');
 
 function App() {
-  const [count, setCount] = useState(0)
+  // TODO need to figure data structure of msgHistory
+  const [msgHistory, setMsgHistory] = useState([
+    { name: 'me1', msg: 'who' },
+    { name: 'me2', msg: 'who2' },
+  ]);
+  const [userMsg, setUserMsg] = useState({
+    name: '',
+    msg: '',
+  });
+
+  const sendMessage = (e) => {
+    console.log(`client ${userMsg.name}: ${userMsg.msg}`);
+    // socket.emit('send_message', { user: userMsg.user, message: userMsg });
+  };
+
+  // refresh whenever socket event happens
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      console.log('from server', data.message);
+      setMsgHistory([...msgHistory, data.message]);
+    });
+  }, [socket]);
 
   return (
     <>
+      <h1>app</h1>
+      <input
+        type="text"
+        placeholder="username here.."
+        onChange={(e) => {
+          setUserMsg({ ...userMsg, name: e.target.value });
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Message here..."
+        onChange={(e) => {
+          setUserMsg({ ...userMsg, msg: e.target.value });
+        }}
+      />
+      <button
+        onClick={(e) => {
+          sendMessage(e);
+        }}
+      >
+        Send Message
+      </button>
+
+      <h2>Messages below:</h2>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {msgHistory.map((user, index) => (
+          <div key={user.user + index}>{`${user.name}: ${user.msg}`}</div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,2 +1,31 @@
-const app = require('express');
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
 const cors = require('cors');
+
+// middleware
+app.use(cors());
+
+// socket.io requires an http server even if express can do it
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+});
+
+//connection event
+io.on('connection', (socket) => {
+  console.log(`User Connected ${socket.id}`);
+
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('webserver listening on: 3000');
+});
